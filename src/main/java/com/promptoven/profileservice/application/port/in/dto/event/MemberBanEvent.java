a0836932951bf4e77d3b0dfa79e92b2e5651ebba
yuseok-kim-edushare <yuseok.kim@edushare.kr>
2024-12-05 10:53:31 +0900

    🐛 Fix: event dto add NoArgConstructor to proper consume

--- src/main/java/com/promptoven/profileservice/application/port/in/dto/event/MemberBanEvent.java
+++ src/main/java/com/promptoven/profileservice/application/port/in/dto/event/MemberBanEvent.java
@@ -3,9 +3,11 @@ package com.promptoven.profileservice.application.port.in.dto.event;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class MemberBanEvent {
 
